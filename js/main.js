3491068bde57413b306d5c0d44dbaa0445a73c1c
juweek <juweek.adolphe@gmail.com>
2022-04-26 23:04:55 -0400

    data stuff

--- js/main.js
+++ js/main.js
@@ -19,33 +19,24 @@ var arr = Array.from(svgPaths)
 METHOD: set the size of the canvas
 ------------------------------
 */
-let height = 600;
+let height = 200;
 let width = 1000;
 let margin = ({top: 0, right: 40, bottom: 34, left: 40});
 
-let Scales = {
-  lin: "scaleLinear",
-  log: "scaleLog"
-};
 
 // Data structure describing volume of displayed data
 let Count = {
   total: "total",
-  perCap: "perCapita"
-};
-
-// Data structure describing legend fields value
-let Legend = {
-  total: "Total Deaths",
-  perCap: "Per Capita Deaths"
+  perCap: "perCapita",
+  population: "Average income"
 };
 
 
 let chartState = {};
 
 chartState.measure = Count.total;
-chartState.scale = Scales.lin;
-chartState.legend = Legend.total;
+chartState.scale = "scaleLinear";
+
 
 // Colors used for circles depending on continent
 let colors = d3.scaleOrdinal()
@@ -59,11 +50,22 @@ d3.select("#southAmericaColor").style("color", colors("southAmerica"));
 d3.select("#europeColor").style("color", colors("europe"));
 d3.select("#oceaniaColor").style("color", colors("oceania"));
 
+/*
+------------------------------
+METHOD: select the d3 div and set the width and height
+------------------------------
+*/
+
 let svg = d3.select("#svganchor")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
+/*
+------------------------------
+METHOD: add in the x-axis
+------------------------------
+*/
 let xScale = d3.scaleLinear()
     .range([margin.left, width - margin.right]);
 
@@ -76,54 +78,55 @@ let xLine = svg.append("line")
     .attr("stroke", "rgb(96,125,139)")
     .attr("stroke-dasharray", "1,2");
 
+// Create tooltip div and make it invisible
+let tooltip = d3.select("#svganchor").append("div")
+.attr("class", "tooltip")
+.style("opacity", 0);
 
-// Load and process data
+/*
+------------------------------
+METHOD: load in and process data
+------------------------------
+*/
 d3.csv("https://raw.githubusercontent.com/juweek/beeswarm/main/top20Counties.csv").then(function (data) {
 
     let dataSet = data;
 
-    //console.log(dataSet)
-
-    // Set chart domain max value to the highest total value in data set
+    // Set chart domain max value to the highest total value in data set. this will affect the key
     xScale.domain(d3.extent(data, function (d) {
         return +d.total;
     }));
 
     redraw();
 
-
     function redraw() {
 
-        // Set scale type based on button clicked
-        if (chartState.scale === Scales.lin) {
-            xScale = d3.scaleLinear().range([ margin.left, width - margin.right ])
-        }
-
-        if (chartState.scale === Scales.log) {
-            xScale = d3.scaleLog().range([ margin.left, width - margin.right ]);
-        }
-
+        //set the scale based off the range from the dataset
+        xScale = d3.scaleLinear().range([ margin.left, width - margin.right ])
+       
         xScale.domain(d3.extent(dataSet, function(d) {
             return +d[chartState.measure];
         }));
 
-        let xAxis;
         // Set X axis based on new scale. If chart is set to "per capita" use numbers with one decimal point
+        let xAxis;
         if (chartState.measure === Count.perCap) {
             xAxis = d3.axisBottom(xScale)
-                .ticks(10, ".1f")
+                .ticks(7, ".1f")
                 .tickSizeOuter(0);
         }
         else {
             xAxis = d3.axisBottom(xScale)
-                .ticks(10, ".1s")
+                .ticks(7, ".1s")
                 .tickSizeOuter(0);
         }
 
+        //include a transition for the x axis if you change the scale
         d3.transition(svg).select(".x.axis")
             .transition()
             .duration(1000)
             .call(xAxis);
+
 
         // Create simulation with specified dataset
         let simulation = d3.forceSimulation(dataSet)
@@ -152,12 +155,14 @@ d3.csv("https://raw.githubusercontent.com/juweek/beeswarm/main/top20Counties.csv
             .attr("cy", (height / 2) - margin.bottom / 2)
             .remove();
 
+        //for every FIPS in the dataset, create a circle, set the r, and set the circles
         countriesCircles.enter()
             .append("circle")
             .attr("class", "countries")
             .attr("cx", 0)
             .attr("cy", (height / 2) - margin.bottom / 2)
-            .attr("r", 2.5)
+            //.attr("r", 4)
+            .attr("r", (function(d){ return (parseInt(d.population))}))
            // .attr("fill", function(d){return colors(d.Color)})
             .attr("fill", function(d){return d.Color})
             .merge(countriesCircles)
@@ -166,7 +171,29 @@ d3.csv("https://raw.githubusercontent.com/juweek/beeswarm/main/top20Counties.csv
             .attr("cx", function(d) { return d.x; })
             .attr("cy", function(d) { return d.y; });
 
+               // Show tooltip when hovering over circle (data for respective country)
+        d3.selectAll(".countries").on("mousemove", function(d) {
+            tooltip.html(`Country: <strong>${d.country}</strong><br>
+                          ${chartState.legend.slice(0, chartState.legend.indexOf(","))}: 
+                          <strong>${d3.format(",")(d[chartState.measure])}</strong>
+                          ${chartState.legend.slice(chartState.legend.lastIndexOf(" "))}`)
+                .style('top', d3.event.pageY - 12 + 'px')
+                .style('left', d3.event.pageX + 25 + 'px')
+                .style("opacity", 0.9``);
+
+            xLine.attr("x1", d3.select(this).attr("cx"))
+                .attr("y1", d3.select(this).attr("cy"))
+                .attr("y2", (height - margin.bottom))
+                .attr("x2",  d3.select(this).attr("cx"))
+                .attr("opacity", 1);
+
+        }).on("mouseout", function(_) {
+            tooltip.style("opacity", 0);
+            xLine.attr("opacity", 0);
+        });
+
     }
+
 
     // Filter data based on which checkboxes are ticked
     function filter() {
